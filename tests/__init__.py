64772510302d09735b1b4ca781cd89f9eb8520d1
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 12:44:49 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-try:
-    from trytond.modules.account_invoice_order_number.tests.test_account_invoice_order_number import (
-        suite)
-except ImportError:
-    from .test_account_invoice_order_number import suite
-
-__all__ = ['suite']
